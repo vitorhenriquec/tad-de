2c0cb494110dea59909f919a54091d2463f9d65c
Vitor Henrique <vitorhenrique908@gmail.com>
2018-09-09 10:10:22 -0300

    fixing LinkedList

--- LinkedList/include/linkedlist.hpp
+++ LinkedList/include/linkedlist.hpp
@@ -6,6 +6,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+#include <stdlib.h>
+
 template<class T>
 class LinkedList{
 	private:
@@ -41,30 +43,94 @@ class LinkedList{
 		unsigned size(){
 			return tam;
 		}
-		void insertBegging(T chave_){
+		bool empty(){
+			return this->tam == 0;
+		}
+		void insertBeginning(T chave_){
 			Node * novo = new Node(chave_);
-			novo->next = this->inicio;
-			novo->prev = this->fim;
-			this->fim->next = novo;
-			this->inicio->prev = novo;
-			this->inicio = novo;
+			if(empty()){
+				novo->next = novo;
+				novo->prev = novo;
+				this->fim = novo;
+				this->inicio = novo;
+			}
+			else{
+				novo->next = this->inicio;
+				novo->prev = this->fim;
+				this->fim->next = novo;
+				this->inicio->prev = novo;
+				this->inicio = novo;
+			}
 			tam+=1;
 
 		}
 		void removeEnd(){
-			Node * tmp = this->fim;
-			this->fim->prev->next = this->inicio;
-			this->fim =this->fim->prev;
-			this->inicio->prev = this->fim;
-			delete tmp;
+			if(empty()){
+				cout << "List vazia" << endl;
+				 exit(EXIT_FAILURE);
+			}
+			else if(this->tam == 1){
+				Node * tmp = this->fim;
+				this->fim = nullptr;
+				this->inicio = nullptr;
+				delete tmp;
+			}
+			else{
+				Node * tmp = this->fim;
+				this->fim->prev->next = this->inicio;
+				this->fim =this->fim->prev;
+				this->inicio->prev = this->fim;
+				delete tmp;
+			}
 			tam-=1;
 		}
 		void print(){
-			//cout << "Tam:" << tam<< endl; 
+			if(empty()){
+				cout << "Lista vazia" << endl;
+				 exit(EXIT_FAILURE);
+			} 
 			Node * it = this->inicio;
-			while(it->next != this->fim){
+			do{
 				cout << it->chave << endl;
 				it = it->next;
+			}while(it!= this->inicio);
+		}
+		void swap(T a, T b){
+			if(empty()){
+				cout << "Lista vazia" << endl;
+				 exit(EXIT_FAILURE);
+			}
+			else{
+				if(a == b){
+					 exit(EXIT_FAILURE);
+				}
+				Node * x = this->inicio;
+				Node * y = this->inicio;
+				bool xx = 0, yy = 0;
+				do{
+					if(x->chave == a){
+						xx = 1;
+						break;
+					}
+					x = x->next;
+				}while(x!=this->inicio);
+
+				do{
+					if(y->chave == b){
+						yy = 1;
+						break;
+					}
+					y = y->next;
+				}while(y!=this->inicio);
+				if(xx && yy){
+					T tmp = x->chave;
+					x->chave = y->chave;
+					y->chave = tmp;
+				}
+				else{
+					cout << "Um dos elementos não foi encontrado" << endl;
+					 exit(EXIT_FAILURE);
+				}
 			}
 		}
 };
--- LinkedList/src/main.cpp
+++ LinkedList/src/main.cpp
@@ -1,11 +1,12 @@
-#include "../include/linkedList.hpp"
+#include "../include/linkedlist.hpp"
 
 int main(){
 	LinkedList<int> l ;
-	l.insertBegging(2);
-	l.insertBegging(3);
-	l.insertBegging(4);
-	//l->removeEnd();
+	l.insertBeginning(2);
+	l.insertBeginning(3);
+	l.insertBeginning(4);
+	l.removeEnd();
+	//l.swap(4,3);
 	l.print();
 	return 0;
 }
